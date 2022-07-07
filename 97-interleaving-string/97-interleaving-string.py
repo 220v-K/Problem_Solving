81e036cc452c58ae45d8ee71855c63a270799025
Jaewon Lee <jwlee010222@gmail.com>
2022-07-08 04:29:05 +0900

    Time: 83 ms (27.60%), Space: 14.2 MB (58.75%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.py
+++ 97-interleaving-string/97-interleaving-string.py
@@ -1,18 +1,27 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        l1, l2 = len(s1), len(s2)
-        if l1 + l2 != len(s3):
+        leng1 = len(s1)
+        leng2 = len(s2)
+        if leng1 + leng2 != len(s3):
             return False
 
-        last = set([(0, 0)])
-        for char in s3:
-            current = set()
-            for i, j in last:
-                if i < l1 and s1[i] == char:
-                    current.add((i + 1, j))
-                if j < l2 and s2[j] == char:
-                    current.add((i, j + 1))
-            if not current:
+        cursor = set([(0, 0)])
+        for s3char in s3:
+            cursorList = set()
+            isS1 = False
+            isS2 = False
+
+            for n, m in cursor:
+                print(n, m)
+                if n < leng1 and s1[n] == s3char:
+                    cursorList.add((n+1, m))
+                    isS1 = True
+                if m < leng2 and s2[m] == s3char:
+                    cursorList.add((n, m+1))
+                    isS2 = True
+            if not cursorList:
                 return False
-            last = current
+
+            cursor = cursorList
+
         return True
