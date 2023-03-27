312535a276810b5be5f9b5d3a8c985dba979755b
Jaewon Lee <jwlee010222@gmail.com>
2023-03-28 00:55:05 +0900

    [Silver II] Title: 최대 힙, Time: 248 ms, Memory: 126108 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -0,0 +1,17 @@
+import heapq
+
+h = []
+N = int(input())
+nums = []
+for _ in range(N):
+    nums.append(int(input()))
+
+for i in range(N):
+    a = nums[i]
+    if a == 0:
+        if len(h) == 0:
+            print(0)
+        else:
+            print(-heapq.heappop(h))
+    else:
+        heapq.heappush(h, -a)
