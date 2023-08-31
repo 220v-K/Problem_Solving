a1ecb444aabf7915f3609eb556071b8469d1d676
220v-K <jwlee010222@gmail.com>
2023-08-31 12:44:04 +0900

    update 풀이

---
+++ 프로그래머스/Programmers_고득점Kit/42587. 프로세스/프로세스.py
@@ -0,0 +1,18 @@
+from collections import deque
+def solution(priorities, location):
+    # target location 표시
+    prior = [[i, 0] for i in priorities]
+    prior[location][1] = 1
+
+    deq = deque(prior)
+    answer = 0
+
+    while True:
+        if deq[0][0] == max(deq, key=lambda x: x[0])[0]:
+            if deq[0][1] == 1:
+                return answer + 1
+            else:
+                deq.popleft()
+                answer += 1
+        else:
+            deq.rotate(-1)
